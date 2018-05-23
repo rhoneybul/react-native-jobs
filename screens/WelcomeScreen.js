f01a96e36bbe1d8ce11538c45b0ffca146918848
Robert Honeybul <robert.honeybul@rthdatasolutions.com>
2018-05-23 16:30:48 +0800

    map component added

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, AsyncStorage } from 'react-native';
 import Slides from '../components/Slides';
+import { AppLoading } from 'expo';
+import _ from 'lodash';
 
 const SLIDE_DATA = [
     { text: "Welcome to JobApp", color: "#03A9F4" }, 
@@ -9,13 +11,28 @@ const SLIDE_DATA = [
 ];
 
 class WelcomeScreen extends Component {
-    state = {  }
+    state = { token: null }
 
+    async componentWillMount () {
+        let token = await AsyncStorage.getItem('fb_token');
+        
+        if (token) {
+            this.props.navigation.navigate('map');
+            this.setState({ token: false });
+        } else {
+            this.setState({ token: false })
+        }
+    }
+    
     onComplete() {
         this.props.navigation.navigate('auth');
     }
 
     render() {
+        if(_.isNull(this.state.token)) {
+            return <AppLoading />
+        }
+
         return (
             <Slides data={SLIDE_DATA} 
                     onComplete={this.onComplete.bind(this)}
